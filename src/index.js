0afa8ea23e1b5d29c8e95ccce0540c798c47ed33
KHong <albtrossfacefall@gmail.com>
2019-11-28 10:55:39 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function() {
+  talkToImagesApi();
+});
+
+function talkToImagesApi() {
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(resp => resp.json())
+  .then(json => getImages(json));
+}
+
+function getImages(json) {
+  for (let i=0; i<json.message.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.src = json.message[i];
+    document.body.appendChild(newImage);
+  }
+}
