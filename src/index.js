ed946c0b3c44ca04b23bab1969e3056cb1f50c3d
KHong <albtrossfacefall@gmail.com>
2019-11-28 13:12:19 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ console.log('%c HI', 'color: firebrick')
 
 document.addEventListener('DOMContentLoaded', function() {
   talkToImagesApi();
+  talkToBreedsApi();
+  addFilter();
 });
 
 function talkToImagesApi() {
@@ -16,4 +18,66 @@ function getImages(json) {
     newImage.src = json.message[i];
     document.body.appendChild(newImage);
   }
+}
+
+function talkToBreedsApi() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(resp => resp.json())
+  .then(json => getDogBreeds(json.message));
+}
+
+function getDogBreeds(json) {
+  let nameArray = [];
+  const breedList = document.getElementById('dog-breeds');
+  for (const name in json) {
+    if (json[name].length !== 0) {
+      for (let i=0; i<json[name].length; i++) {
+        nameArray.push(`${json[name][i]} ${name}`);
+      }
+    } else {
+      nameArray.push(name)
+    }
+  }
+  for (let i=0; i<nameArray.length; i++) {
+    const newDogBreed = document.createElement('li');
+    newDogBreed.innerText = nameArray[i];
+    addColorChange(newDogBreed);
+    breedList.appendChild(newDogBreed);
+  }
+}
+
+function addColorChange(li) {
+  li.addEventListener('click', function() {
+    if (li.style.color==='red') {
+      li.style.color = '';
+    } else {
+      li.style.color = 'red';
+    }
+  });
+}
+
+function addFilter() {
+  const dropDown = document.getElementById('breed-dropdown');
+  const breedList = document.getElementById('dog-breeds');
+  const holderArray = [];
+  dropDown.addEventListener('change', function() {
+
+    for (let i=0; i<holderArray.length; i++){
+      breedList.appendChild(holderArray[i]);
+    }
+
+    holderArray.length=0;
+
+    for (let i=0; i<breedList.childElementCount; i++) {
+      let thisChild = breedList.children[i]
+      if (!thisChild.innerText.startsWith(dropDown.value)) {
+        holderArray.push(thisChild);
+      }
+    }
+
+    for (let i=0; i<holderArray.length; i++) {
+      breedList.removeChild(holderArray[i]);
+    }
+
+  });
 }
